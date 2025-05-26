f289fdbb0a80c98e81cd00394f146fb6622d3dab
Echotiized <13004056+Echotiized@users.noreply.github.com>
2025-05-26 16:09:50 -0400

    Wrote the code for ties, wins, and losses.

--- javascript.js
+++ javascript.js
@@ -8,4 +8,25 @@ let rps = (choice) => {
     }
 
     let computerChoice = validChoices[Math.floor(Math.random() * validChoices.length)];
-}
+
+    if (choice === computerChoice) {
+        console.log(`It's a tie! You chose ${choice} and the computer chose ${computerChoice}.`);
+    return;
+    }
+
+    if (choice === "rock" && computerChoice === "scissors"
+        || choice === "scissors" && computerChoice === "paper"
+        || choice === "paper" && computerChoice === "rock"
+    ) {
+        console.log(`You won! You chose ${choice} and the computer chose ${computerChoice}.`);
+        return;
+    }
+
+     if (choice === "rock" && computerChoice === "paper"
+        || choice === "scissors" && computerChoice === "rock"
+        || choice === "paper" && computerChoice === "scissors"
+    ) {
+        console.log(`You lost! You chose ${choice} and the computer chose ${computerChoice}.`);
+        return;
+    }
+}   
